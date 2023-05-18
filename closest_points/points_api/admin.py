a46316d5622346615ddf1fad999fa38a93def786
BabGee <jeremiah.omondi@interintel.co.ke>
2023-05-18 14:46:48 +0300

    register Point Model on admin

--- closest_points/points_api/admin.py
+++ closest_points/points_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from points_api.models import Point
+
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('x', 'y')
+
+admin.site.register(Point, PointAdmin)
